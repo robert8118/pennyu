700f7b7c0ea1cfc4c0dbe502380e60b0f22ac55e
JoenanBurgundy <joenannr@gmail.com>
2020-07-22 14:53:07 +0700

    [FIX] sale_invoice_amount

--- sale_invoice_amount/models/sale.py
+++ sale_invoice_amount/models/sale.py
@@ -7,18 +7,18 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     invoiced_amount = fields.Monetary(
-        string='Invoiced', store=True, readonly=True, compute='_compute_invoice_amount')
+        string='Invoiced', store=True, readonly=True, compute='_compute_invoice_amount', compute_sudo=True)
     balance_amount = fields.Monetary(
-        string='Balance', store=True, readonly=True, compute='_compute_invoice_amount')
+        string='Balance', store=True, readonly=True, compute='_compute_invoice_amount', compute_sudo=True)
 
-    @api.one
     @api.depends('order_line.qty_invoiced')
     def _compute_invoice_amount(self):
-        sale = self.sudo()
-        invoiced_amount = sum(inv.amount_total for inv in sale.invoice_ids.filtered(
-            lambda x: x.state in ('draft', 'open', 'paid') and x.type == 'out_invoice'))
-        if invoiced_amount:
-            sale.invoiced_amount = invoiced_amount
-            sale.balance_amount = sale.amount_total - invoiced_amount
-        else:
-            sale.balance_amount = sale.amount_total
+        for sale in self:
+            invoiced_amount = sum(inv.amount_total for inv in sale.invoice_ids.filtered(
+                lambda x: x.state in ('draft', 'open', 'paid') and x.type == 'out_invoice'))
+            if invoiced_amount:
+                sale.invoiced_amount = invoiced_amount
+                sale.balance_amount = sale.amount_total - invoiced_amount
+            else:
+                sale.invoiced_amount = 0.0
+                sale.balance_amount = sale.amount_total
