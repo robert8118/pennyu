6403fa613b163a5971b5b5eb330c5d1dac13e80a
Amirul <muhammad.amirul.muk@gmail.com>
2020-10-23 22:15:07 +0700

    detailing shift in work order

--- pn_mrp_shift/models/mrp_workorder.py
+++ pn_mrp_shift/models/mrp_workorder.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models
 class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
-    mo_shift = fields.Char('Shift', related='production_id.mo_shift.name', readonly=True)
+    mo_shift = fields.Char('Shift', related='production_id.mo_shift.custom_name', readonly=True)
