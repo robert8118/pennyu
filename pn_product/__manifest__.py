102135f9cac716b5f91b8030917a2c286c0660c0
Amirul <muhammad.amirul.muk@gmail.com>
2020-11-14 18:24:31 +0700

    change default route_ids to none when creating new product

---
+++ pn_product/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Pennyu Product',
+    'version': '11.0.1.0.0',
+    'author': 'Amirul',
+    'summary': 'Pennyu Product',
+    'website': '',
+    'category': 'Inventory',
+    'description': """
+    Put custom product here
+    """,
+    'data': [
+
+    ],
+    'depends': ['product', 'purchase'],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
