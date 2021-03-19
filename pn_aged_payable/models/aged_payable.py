fb7ea84a985a5071d3b6b2be93ddb2064b505c7d
Amirul <muhammad.amirul.muk@gmail.com>
2021-03-19 21:00:51 +0700

    [ENHANCE] aged payable: move due date column to after bill number and sort bills by due date

--- pn_aged_payable/models/aged_payable.py
+++ pn_aged_payable/models/aged_payable.py
@@ -5,9 +5,12 @@ class AgedPayableColumn(models.AbstractModel):
     _inherit = 'account.aged.partner'
 
     def get_columns_name(self, options):
+        due_date_cols = {'name': 'Due Date', 'class': 'text', 'style': 'white-space:nowrap;'}
+
         columns = super(AgedPayableColumn, self).get_columns_name(options)
-        columns.append({'name': 'Due Date', 'class': 'number', 'style': 'white-space:nowrap;'})
-        return columns
+        columns2 = columns[:]
+        columns2.insert(1, due_date_cols)
+        return columns2
 
     @api.model
     def get_lines(self, options, line_id=None):
@@ -21,10 +24,33 @@ class AgedPayableColumn(models.AbstractModel):
         info_by_id = build_dict(lines, key="id")
 
         for line in lines:
+            idx = info_by_id.get(line['id']).get('index')
             if not line.get('unfoldable') and line.get('name').strip() != 'Total':
                 sml = self.env['account.move.line'].browse(line['id'])
-                idx = info_by_id.get(line['id']).get('index')
-                lines2[idx]['columns'].append({'name': sml.date_maturity})
+                lines2[idx]['due_date'] = sml.date_maturity
+                old_cols = lines[idx]['columns']
+                lines2[idx]['columns'] = [{'name': sml.date_maturity}]
+                lines2[idx]['columns'].extend(old_cols)
+            else:
+                old_cols = lines[idx]['columns']
+                lines2[idx]['columns'] = [{'name': ''}]
+                lines2[idx]['columns'].extend(old_cols)
+
+        def sort_by_due_date(i):
+            return i['due_date']
+
+        idx_forbid_to_changed = [i for i, v in enumerate(lines2) if
+                                 v.get('unfoldable') or v.get('name').strip() == 'Total']
+        range_idx_need_to_sort = []
+        for i, v in enumerate(idx_forbid_to_changed):
+            if i != len(idx_forbid_to_changed) - 1:
+                if not idx_forbid_to_changed[i + 1] - 1 == v:
+                    range_idx_need_to_sort.append([v + 1, idx_forbid_to_changed[i + 1]])
+
+        for i in range_idx_need_to_sort:
+            sorted_list = lines2[i[0]:i[1]]
+            sorted_list.sort(key=sort_by_due_date, reverse=True)
+            lines2[i[0]:i[1]] = sorted_list
 
         return lines2
 
