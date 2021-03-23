f50e6ee7ccc4401c183ba6001225ee1406ba834c
ardhi <ardhisofyanw@gmail.com>
2021-03-23 13:21:30 +0700

    report dotmatrix 230321 1321

---
+++ aos_report_thermal/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Thermal Report For Pennyuku",
+    "version": "11.0.1.0.0",
+    "depends": ["stock","account_payment","account"],
+    "author": "Alphasoft",
+    "category": "Utilities",
+    'website': 'https://www.alphasoft.co.id',
+    'summary': 'Thermal Reporting',
+    "description": """""",
+    "data": [
+        "report/payment_report.xml",
+        "report/payment_report_template.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
