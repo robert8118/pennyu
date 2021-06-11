6c572b5942cd68cfd4951514279d4eb76ca88395
Amirul <muhammad.amirul.muk@gmail.com>
2021-06-11 16:19:57 +0700

    [UPDATE] block SO: update warning

--- pn_blocking_so/models/sale_order.py
+++ pn_blocking_so/models/sale_order.py
@@ -10,10 +10,10 @@ class SaleOrder(models.Model):
         non_avaiable_product = []
         for line in self.order_line:
             if line.product_uom_qty > line.product_id.virtual_available:
-                non_avaiable_product.append(line.product_id.name)
+                non_avaiable_product.append('- %s. Forcasted Qty: %s' % (line.product_id.name, line.product_id.virtual_available))
         if non_avaiable_product:
-            non_avaiable_product = ', '.join(non_avaiable_product)
-            raise Warning("Can't order product(s): %s because below forecasted quantity" % (
+            non_avaiable_product = '\n'.join(non_avaiable_product)
+            raise Warning("These product(s) has order quantity more than forcasted quantity: \n%s" % (
                 non_avaiable_product))
 
         res = super(SaleOrder, self).action_confirm()
