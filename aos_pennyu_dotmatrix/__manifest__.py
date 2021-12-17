a7cb0ff21f005ed460d7b26d4d249b06b06dba59
Ade <adeanshori@gmail.com>
2021-12-17 13:32:34 +0700

    updat

--- aos_pennyu_dotmatrix/__manifest__.py
+++ aos_pennyu_dotmatrix/__manifest__.py
@@ -1,7 +1,7 @@
 {
 	"name": "Pennyu Dot Matrix Printer",
 	"version": "11.0.1.0.0",
-	"depends": ["vit_dotmatrix", "pn_dotmatrix","stock"],
+	"depends": ["vit_dotmatrix", "pn_dotmatrix","stock", "sale_management", "efaktur"],
 	"author": "Alphasoft",
 	"category": "Utilities",
 	'website': 'https://www.alphasoft.co.id',
