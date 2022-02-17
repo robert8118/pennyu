ef2469bf91b271aa620cb414f260b9b252413f99
Miftahussalam <miftah_salam@ymail.com>
2022-02-17 22:53:57 +0700

    credit_limit: exclude DO yg dicancel

--- credit_limit/models/sale.py
+++ credit_limit/models/sale.py
@@ -16,12 +16,15 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.depends('amt_invoiced')
+    @api.depends('amt_invoiced','move_ids.state')
     def _get_sale_noinvoice(self):
         for rec in self :
             sale_noinvoice = 0
             if rec.order_id.state in ('sale','done'):
-                sale_noinvoice = rec.order_id.currency_id.compute(rec.price_total - rec.amt_invoiced, self.env.user.company_id.currency_id)
+                unit_price = rec.price_total / rec.product_uom_qty if rec.product_uom_qty else 0
+                cancelled_moves = rec.move_ids.filtered(lambda m: m.state == 'cancel')
+                cancelled_amount = sum(cancelled_moves.mapped('product_uom_qty')) * unit_price
+                sale_noinvoice = rec.order_id.currency_id.compute(rec.price_total - cancelled_amount - rec.amt_invoiced, self.env.user.company_id.currency_id)
             rec.amt_noinvoice = sale_noinvoice
 
     amt_noinvoice = fields.Monetary(compute='_get_sale_noinvoice', string='Uninvoiced Amount', store=True)
