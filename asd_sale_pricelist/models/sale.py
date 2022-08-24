d25646299c550aaee7c5949958718104b99e33ed
Syahid Abdillah <syahidabdillah96@gmail.com>
2022-08-24 09:49:41 +0700

    [UPD] asd_sale_pricelist: add method _upd_multi_cat_pricelist to save button

--- asd_sale_pricelist/models/sale.py
+++ asd_sale_pricelist/models/sale.py
@@ -3,11 +3,17 @@ from odoo import api, fields, models, tools, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    @api.multi
+    def write(self, values):
+        res = super(SaleOrder, self).write(values)
+        self._upd_multi_cat_pricelist()
+        return res
+
     def recompute_pricelist(self):
-        self.upd_multi_cat_pricelist()
+        self._upd_multi_cat_pricelist()
     
     # Method to recompute multi cat Pricelist
-    def upd_multi_cat_pricelist(self):
+    def _upd_multi_cat_pricelist(self):
         for line in self.order_line:
             product_context = dict(self.env.context, partner_id=self.partner_id.id, date=self.date_order, uom=line.product_uom.id, sale_order_id=self.id)
             price, rule_id = line.order_id.pricelist_id.with_context(product_context).get_product_price_rule(line.product_id, line.product_uom_qty or 1.0, self.partner_id)
