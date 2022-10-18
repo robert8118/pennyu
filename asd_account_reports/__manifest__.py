0d041fdd1d400a593bc9a1238b79a4c647c4b315
Syahid Abdillah <syahidabdillah96@gmail.com>
2022-10-18 15:39:45 +0700

    [ADD] asd_account_reports

---
+++ asd_account_reports/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Pennyu Account Reports',
+    'summary': """
+        Add some columns to aged payable: Invoice Date, Source Document, Salesperson, and Due Date""",
+    'author': "PT Arkana Solusi Digital",
+    'website': "https://www.arkana.co.id",
+    'category': 'Accounting',
+    'version': '0.1',
+    'data': [
+
+    ],
+    'depends': ['pn_aged_payable'],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
