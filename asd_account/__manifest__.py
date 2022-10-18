17f995835f4c7b5d9ae85aabd74731fc562f0418
Syahid Abdillah <syahidabdillah96@gmail.com>
2022-10-18 14:34:48 +0700

    [ADD] asd_account: add invoice category in register payment

---
+++ asd_account/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PENNYU Account",
+    'summary': """
+        Account for PENNYU V11 EE""",
+    'author': "PT. Arkana Solusi Digital",
+    'website': "http://www.arkana.co.id",
+    'category': 'Account',
+    'version': '0.1',
+    'depends': ['account','payment'],
+    'data': [
+        # 'views/account_move_views.xml',
+        'views/account_payment_views.xml',
+    ],
+}
---
+++ asd_account/models/account_payment.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+    
+    inv_categ = fields.Selection([
+        ('cod', 'COD'),
+        ('tempo', 'Tempo'),
+    ], string='Invoice Category')
+
+    # @api.multi
+    # def _get_move_vals(self, journal=None):
+    #     """ Return dict to create the payment move
+    #     """
+    #     res = super(AccountPayment, self)._get_move_vals()
+    #     res.update({
+    #             'inv_categ': self.inv_categ,
+    #         })
+    #     return res
