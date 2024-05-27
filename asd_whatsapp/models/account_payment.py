743db620998ca733b0b4e381eaf03004b5805cb8
Syahid <syahidabdillah96@gmail.com>
2024-05-27 15:54:56 +0700

    [IMP] asd_whatsapp: improve limit access whatsapp button

--- asd_whatsapp/models/account_payment.py
+++ asd_whatsapp/models/account_payment.py
@@ -1,7 +1,45 @@
-from odoo import models
+from odoo import models, fields, _
+from odoo.exceptions import ValidationError
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
+
+    show_whatsapp_button = fields.Boolean(string='Show Whatsapp Button', compute='_show_whatsapp_button')
+
+    def _get_group_access(self, groups=[]):
+        group_ids = []
+        result = False
+        if not groups:
+            raise ValidationError(_('groups parameter not found. Please add groups parameter first'))
+        for group in groups:
+            group_ids.append(self.env.ref(group).id)
+        if len(group_ids) == 1:
+            filter_query = '= %d' % group_ids[0]
+        elif len(group_ids) > 1:
+            filter_query = 'IN %s' % (tuple(group_ids),)
+        query = """
+            SELECT 
+                1 
+            FROM
+                res_groups_users_rel rgur
+            WHERE 
+                rgur.uid = %(uid)s
+                AND rgur.gid %(gid)s """ % {
+                    'uid': self._uid,
+                    'gid': filter_query
+                }
+        self.env.cr.execute(query)
+        query_result = self.env.cr.fetchone()
+
+        if query_result or self._uid == 1:
+            result = True
+        else:
+            result = False
+        return result
+    
+    def _show_whatsapp_button(self):
+        access_value = self._get_group_access(groups=['account.group_account_invoice', 'account.group_account_manager'])
+        self.show_whatsapp_button = access_value
 
     def send_whatsapp_message(self):
         whatsapp_wizard = self.env['whatsapp.wizard'].create({
