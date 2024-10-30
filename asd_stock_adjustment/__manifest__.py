3cb0b5416d80831c538d8b3dae18a3fc92e966a2
Syahid <syahidabdillah96@gmail.com>
2024-10-30 22:44:24 +0700

    [ADD] asd_stock_adjustment: init

---
+++ asd_stock_adjustment/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 PT Arkana Solusi Digital
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Pennyu - Stock Adjustment",
+    "summary": """Stock Adjustment for Pennyu""",
+    "author": "PT Arkana Solusi Digital",
+    "website": "https://arkana.co.id",
+    "category": "Stock",
+    "version": "0.1",
+    "depends": ["msi_stock_adjustment"],
+    "data": [],
+}
